6eef2601208234c2f43de83376f0f4ada1911f4f
ZaxriddinoV <78430544+ZaxriddinoV@users.noreply.github.com>
2025-03-02 22:58:19 +0500

    Initial commit

---
+++ src/test/java/com/company/topnews/TopnewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.topnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopnewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
